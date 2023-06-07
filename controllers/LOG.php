14d3cf8e61a3493ed10989d1530c217d0856b1da
Volodymyr_dev <vfylyp@gmail.com>
2023-06-07 15:51:17 +0300

    fixed log

--- controllers/LOG.php
+++ controllers/LOG.php
@@ -1,14 +1,35 @@
 <?php namespace RegisterForm;
 
 class LOG{
-    private static $log_file_path = "/logs";
+    private static $log_folder = "/logs";
 
     public static function RegisterLog($message){
-        $file_name  = $_SERVER['DOCUMENT_ROOT'].static::$log_file_path.'/Register_log.txt';
-        $file       = fopen($file_name, 'a');
+        $log_path   = $_SERVER['DOCUMENT_ROOT'].static::$log_folder;
+        $file_name  = $log_path.'/Register_log.txt';
 
-        fwrite($file, $message);
-        fclose($file);
+        static::prepareFolder($log_path);
+        static::prepareFile($file_name);
+
+        $file = fopen($file_name, 'a');
+
+        if($file){
+            fwrite($file, $message);
+            fclose($file);
+        }
+
+    }
+
+    public static function prepareFolder($folder){
+        if(!is_dir($folder)){
+            mkdir($folder, 0777);
+        }
+    }
+
+    public static function prepareFile($file_name){
+        if(!file_exists($file_name)){
+            $file = fopen($file_name, 'w');
+            fclose($file);
+        }
     }
 }
 class_alias('RegisterForm\LOG', 'LOG');
